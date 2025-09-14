937cc668b6840866a3126a4d2db58cea9b7e2993
bakhodir_abdullaev <bahodirabdullayev0556@gmail.com>
2025-09-14 15:43:16 +0500

    Refactor Showcase component for consistent styling and import order

--- src/app/components/showcase.tsx
+++ src/app/components/showcase.tsx
@@ -1,9 +1,9 @@
-import { FC, PropsWithChildren } from "react";
-import { greatVibes } from "@/fonts";
-import Flowers from "./flowers";
-import Logo from "./logo";
-import Line from "./line";
-import ScrollDown from "./scroll-down";
+import { FC, PropsWithChildren } from 'react';
+import { fleurDeLeah, greatVibes } from '@/fonts';
+import Flowers from './flowers';
+import Logo from './logo';
+import Line from './line';
+import ScrollDown from './scroll-down';
 
 interface ShowcaseProps {
   couple: {
@@ -19,10 +19,10 @@ const Showcase: FC<PropsWithChildren<ShowcaseProps>> = ({
 }: ShowcaseProps): JSX.Element => {
   console.log(man, woman);
   return (
-    <div className="h-svh flex flex-col gap-5">
+    <div className='h-svh flex flex-col gap-5'>
       <nav
-        className="flex items-center justify-between p-4 px-8 mb-20"
-        data-aos="fade-down"
+        className='flex items-center justify-between p-4 px-8 mb-20'
+        data-aos='fade-down'
       >
         <Logo firstLetter={logo[0]} secondLetter={logo[1]} />
         <div
@@ -32,20 +32,23 @@ const Showcase: FC<PropsWithChildren<ShowcaseProps>> = ({
         </div>
       </nav>
       <div
-        className={`${greatVibes.className} relative text-6xl [text-shadow:2px_2px_2px_#6b6b6b] py-5 text-sky-600 space-y-5`}
+        className={`${fleurDeLeah.className} relative text-6xl [text-shadow:2px_2px_2px_#6b6b6b] py-5 text-sky-600 space-y-5`}
       >
         <Flowers />
-        <h1 className="pl-10" data-aos="fade-right">
+        <h1 className='pl-10' data-aos='fade-right'>
           {man}
         </h1>
-        <h1 className="text-center" data-aos="zoom-in">
+        <h1
+          className={`${greatVibes.className} text-center`}
+          data-aos='zoom-in'
+        >
           &
         </h1>
-        <h1 className="pr-10 flex justify-end" data-aos="fade-left">
+        <h1 className='pr-10 flex justify-end' data-aos='fade-left'>
           {woman}
         </h1>
       </div>
-      <div className="flex-1 flex flex-col items-center justify-between pt-16">
+      <div className='flex-1 flex flex-col items-center justify-between pt-16'>
         <Line />
         <ScrollDown />
       </div>
